185c0710821eb829a4c9c5641edbfdef11140ef7
PeaWarrior <jacksonchen171@gmail.com>
2021-04-25 14:48:42 -0400

    add nutrition fact details

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const URL = 'https://www.heb.com/category/shop/pantry/cereal-breakfast/cereal/49
   });
 
   const cereals = [];
-  for (link of cerealLinks.slice(0, 2)) {
+  for (link of cerealLinks.slice(0, 1)) {
     await page.goto(link, { waitUntil: 'networkidle2' });
     const data = await page.evaluate(() => {
       const cereal = {};
@@ -24,18 +24,51 @@ const URL = 'https://www.heb.com/category/shop/pantry/cereal-breakfast/cereal/49
       const size = document.querySelector('div.packing-options').innerText.trim();
       const price = document.querySelector('span#addToCartPrice').innerText.trim();
       const description = document.querySelector('p').innerText.trim();
+      const nutritionFacts = document.querySelector('li.liNutrition > div.content').children;
+      const servingsPerContainer = nutritionFacts[0].innerText.trim().split('\n')[0];
+      const servingSize = nutritionFacts[1].querySelector('tbody > tr > td.val').innerText.trim();
+      const details = nutritionFacts[2].querySelector('table.details-xtd').children;
+      // const details = nutritionFacts[2].querySelectorAll('tr');
+
+      const nF = []
+      details.forEach(node => {
+        const row = node.querySelector('tr').children;
+        const nutrient = row[0].innerText.replace(/\s+/g, " ").trim();
+        const value = row[1].innerText.trim();
+
+        const detail = {};
+        detail[nutrient] = value;
+
+        if (node.nodeName === 'TBODY') {
+          if (nF[nF.length - 1].sub) {
+            nF[nF.length - 1].sub.push(detail);
+          } else {
+            nF[nF.length - 1].sub = [detail]
+          }
+        } else {
+          nF.push(detail);
+        };
+      });
 
       cereal.name = name;
       cereal.size = size;
       cereal.price = price;
       cereal.description = description;
       cereal.img = img;
+      cereal.nutritionFacts = {
+        servingsPerContainer: servingsPerContainer,
+        servingSize: servingSize,
+        details: nF,
+      }
 
       return cereal;
     })
     cereals.push(data);
   }
   console.log(cereals);
+  console.log(cereals[0].nutritionFacts.details);
+  console.log(cereals[0].nutritionFacts.details[0].sub);
+  console.log(cereals[0].nutritionFacts.details[4].sub);
 
   await browser.close();
 })();
