1cc6ddf71a50eb714263329a2a8bf7f0f66a5599
PeaWarrior <jacksonchen171@gmail.com>
2021-05-01 15:48:42 -0400

    skip if table missing & scraped data.json

--- app.js
+++ app.js
@@ -1,104 +1,116 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const URL = 'https://www.heb.com/category/shop/pantry/cereal-breakfast/cereal/490116/490560';
+let URL = 'https://www.heb.com/category/shop/pantry/cereal-breakfast/cereal/490116/490560';
 
 (async () => {
-  const browser = await puppeteer.launch( );
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto(URL, { waitUntil: 'networkidle2' });
-  const cerealLinks = await page.evaluate(() => {
-    const links = [];
-    const nodes = document.querySelectorAll('ul#productResults > li > a');
-    nodes.forEach(node => {
-      if (!node['href'].includes('h-e-b-') && !node['href'].includes('raisin-bran')) links.push(node['href']);
-    });
-    return links;
-  });
 
   const cereals = [];
-  for (link of cerealLinks) {
-    await page.goto(link, { waitUntil: 'networkidle2' });
-    const data = await page.evaluate(() => {
-      const cereal = {};
-      const name = document.querySelector('h1').innerText.trim();
-      const img = document.querySelector('img.pdp-mobile-image').src.split('?')[0];
-      const weight = document.querySelector('div.packing-options').innerText.trim();
-      const price = document.querySelector('span#addToCartPrice').innerText.trim();
-      const description = document.querySelector('p').innerText.trim();
-      const calorieDetails = document.querySelector('table.details-single').querySelectorAll('.val');
-      const nutritionFactDetails = document.querySelector('table.details-xtd').children;
-      const vitaminDetails = document.querySelector('table.vitamins > tbody').children;
-      let servingsPerContainer;
-      let servingSize;
-      
-      const calories = parseInt(calorieDetails[0].innerText);
-      const caloriesFromFat = calorieDetails[1] ? parseInt(calorieDetails[1].innerText): 0;
-      
-      servingsPerContainer = document.querySelector('div.content > p').innerText.match(/(about\s+)\d+/i) || document.querySelector('div.content > p').innerText.match(/(?<=container\s+)[.\d]+/i);
-      servingSize = document.querySelector('div.content').innerText.match(/[ ./\d]+ cup[s]?[\s(\dg]+\)?/) || ['1'];
-
-      const details = [];
-      nutritionFactDetails.forEach(node => {
-        const row = node.querySelector('tr').children;
-        const nutrient = row[0].innerText.replace(/\s+/g, " ").trim();
-        const value = row[1].innerText.trim();
-
-        const detail = {};
-        detail.name = nutrient;
-        detail.dv = value;
-
-        if (node.nodeName === 'TBODY') {
-          if (details[details.length - 1].sub) {
-            details[details.length - 1].sub.push(detail);
-          } else {
-            details[details.length - 1].sub = [detail];
-          }
-        } else {
-          details.push(detail);
-        };
+  while (URL) {
+    await page.goto(URL, { waitUntil: 'networkidle2' });
+    const cerealLinks = await page.evaluate(() => {
+      const links = [];
+      const nodes = document.querySelectorAll('ul#productResults > li > a');
+      nodes.forEach(node => {
+        if (!node['href'].includes('h-e-b-') && !node['href'].includes('raisin-bran')) links.push(node['href']);
       });
-
-      const vitamins = [];
-      let doubleColumn = false;
-      vitaminDetails.forEach(node => {
-        const row = node.children;
-        if (doubleColumn || row[0].innerText.includes('%')) {
-          doubleColumn = true;
-          row.forEach(vit => {
-            const detail = {};
-            detail.name = vit.innerText.match(/[A-Z]+ ?([\w\d]+)?/i)[0];
-            detail.dv = vit.innerText.match(/\d+\%/) ? vit.innerText.match(/\d+\%/)[0] : '0%';
-            vitamins.push(detail);
-          })
-        } else {
-          const vitamin = row[0].innerText.match(/[A-Z]+ ?([\w\d]+)?/i)[0];
-          const value = row[1].innerText.trim() || '0%';
-  
-          const detail = {};
-          detail.name = vitamin;
-          detail.dv = value;
-          vitamins.push(detail);
-        }
-      });
-
-      cereal.name = name;
-      cereal.weight = weight;
-      cereal.price = price;
-      cereal.description = description;
-      cereal.img = img;
-      cereal.nutritionFacts = {
-        servingsPerContainer: servingsPerContainer ? servingsPerContainer[0].trim() : null,
-        servingSize: servingSize[0].trim() || null,
-        calories: calories,
-        caloriesFromFat: caloriesFromFat,
-        details: details,
-        vitamins: vitamins,
-      };
-
-      return cereal;
+      return links;
     });
 
-    cereals.push(data);
+    const nextPage = await page.evaluate(() => {
+      const a = document.querySelector('[aria-label="go to next page"]');
+      return a ? a.href : null;
+    });
+    URL = nextPage ? nextPage : null;
+    
+    for (link of cerealLinks) {
+      await page.goto(link, { waitUntil: 'networkidle2' });
+      const data = await page.evaluate(() => {
+        const cereal = {};
+        const name = document.querySelector('h1').innerText.trim();
+        const img = document.querySelector('img.pdp-mobile-image').src.split('?')[0];
+        const weight = document.querySelector('div.packing-options').innerText.trim();
+        const price = document.querySelector('span#addToCartPrice').innerText.trim();
+        const description = document.querySelector('p').innerText.trim();
+
+        const table = document.querySelector('table');
+        if (!table) return;
+        const calorieDetails = document.querySelector('table.details-single').querySelectorAll('.val');
+        const nutritionFactDetails = document.querySelector('table.details-xtd').children;
+        const vitaminDetails = document.querySelector('table.vitamins > tbody').children;
+        let servingsPerContainer;
+        let servingSize;
+        
+        const calories = parseInt(calorieDetails[0].innerText);
+        const caloriesFromFat = calorieDetails[1] ? parseInt(calorieDetails[1].innerText): 0;
+        
+        servingsPerContainer = document.querySelector('div.content > p').innerText.match(/(about\s+)\d+/i) || document.querySelector('div.content > p').innerText.match(/(?<=container\s+)[.\d]+/i);
+        servingSize = document.querySelector('div.content').innerText.match(/[ ./\d]+ cup[s]?[\s(\dg]+\)?/) || ['1'];
+  
+        const details = [];
+        nutritionFactDetails.forEach(node => {
+          const row = node.querySelector('tr').children;
+          const nutrient = row[0].innerText.replace(/\s+/g, " ").trim();
+          const value = row[1].innerText.trim();
+  
+          const detail = {};
+          detail.name = nutrient;
+          detail.dv = value;
+  
+          if (node.nodeName === 'TBODY') {
+            if (details[details.length - 1].sub) {
+              details[details.length - 1].sub.push(detail);
+            } else {
+              details[details.length - 1].sub = [detail];
+            }
+          } else {
+            details.push(detail);
+          };
+        });
+  
+        const vitamins = [];
+        let doubleColumn = false;
+        vitaminDetails.forEach(node => {
+          const row = node.children;
+          if (doubleColumn || row[0].innerText.includes('%')) {
+            doubleColumn = true;
+            row.forEach(vit => {
+              const detail = {};
+              detail.name = vit.innerText.match(/[A-Z]+ ?([\w\d]+)?/i)[0];
+              detail.dv = vit.innerText.match(/\d+\%/) ? vit.innerText.match(/\d+\%/)[0] : '0%';
+              vitamins.push(detail);
+            })
+          } else {
+            const vitamin = row[0].innerText.match(/[A-Z]+ ?([\w\d]+)?/i)[0];
+            const value = row[1].innerText.trim() || '0%';
+    
+            const detail = {};
+            detail.name = vitamin;
+            detail.dv = value;
+            vitamins.push(detail);
+          }
+        });
+  
+        cereal.name = name;
+        cereal.weight = weight;
+        cereal.price = price;
+        cereal.description = description;
+        cereal.img = img;
+        cereal.nutritionFacts = {
+          servingsPerContainer: servingsPerContainer ? servingsPerContainer[0].trim() : null,
+          servingSize: servingSize[0].trim() || null,
+          calories: calories,
+          caloriesFromFat: caloriesFromFat,
+          details: details,
+          vitamins: vitamins,
+        };
+  
+        return cereal;
+      });
+      
+      if (data) cereals.push(data);
+    };
   };
 
   fs.writeFile('data.json', JSON.stringify(cereals), { flag: 'w' }, err => console.log(err));
